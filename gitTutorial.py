1880591bec5ff341ae6cc7c278bead515786bcd6
JY Park <jeyoungp@andrew.cmu.edu>
2021-07-18 17:01:43 +0900

    This is git tutorial

---
+++ gitTutorial.py
@@ -0,0 +1,27 @@
+
+print("This is a git tutorial")
+
+
+
+
+
+
+
+
+
+
+#  Begin/Initialize the process 
+#  Add items to the basket  
+#  Write a message 
+#  Push the gift 
+	# first time - Address 
+	# first time - Confirmation 
+
+
+
+
+
+
+
+
+
